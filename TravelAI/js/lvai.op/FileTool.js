e9c6ca7d3d5b7ceb3a1e3879c10fd5ff31ced98f
kaixuan <765668947@qq.com>
2015-10-23 15:46:49 +0800

    添加缩略图接口，修复头像没有上传就取消时显示的问题
    添加添加缩略图接口，修改检查文件，以及文件下载接口

--- TravelAI/js/lvai.op/FileTool.js
+++ TravelAI/js/lvai.op/FileTool.js
@@ -2,14 +2,40 @@ var fileTool = {
 
 	//检验文件是否存在
 	//paths: 文件路径例如："_www/test.html"
-	fileExist: function(paths, successHandler, faildHandler) {
-		plus.io.resolveLocalFileSystemURL(paths, successHandler, faildHandler);
+	//serverPath: 服务器文件地址
+	fileExist: function(paths, serverPath, successHandler, faildHandler) {
+		plus.io.resolveLocalFileSystemURL(paths, function() {
+			successHandler(paths);
+		}, function() {
+			faildHandler(paths, serverPath);
+		});
 	},
-	fileDown: function(paths, successHandler, faildHandler) {
+	//文件的下载
+	//paths: 服务器文件路径
+	//types: 服务器基本地址
+	fileDown: function(paths, types, successHandler, faildHandler) {
+		var fileDir = "_doc/HeadPhoto/";
+		switch (types) {
+			case 1:
+				{
+					fileDir = "_doc/HeadPhoto/";
+					break;
+				}
+			case 2:
+				{
+					fileDir = "_doc/CarPhoto/";
+					break;
+				}
+			case 3:
+				{
+					fileDir = "_doc/CarPhoto/thumbCarPhoto/"
+					break;
+				}
+		};
 		var dtask = null;
 		var options = {
 			method: "GET",
-			filename: "_doc/HeadPhoto/"
+			filename: fileDir
 		};
 		dtask = plus.downloader.createDownload(paths, options);
 		dtask.addEventListener("statechanged", function(task, status) {
@@ -31,5 +57,19 @@ var fileTool = {
 		});
 		dtask.start();
 	},
-
+	//图片文件的压缩
+	//existPath：存在的地址 savePath：保存的地址
+	ImgFileCompress: function(existPath, savePath, successHandler, faildHandler) {
+		plus.zip.compressImage({
+			quality: 100,
+			overwrite: true,
+			src: existPath,
+			dst: savePath,
+			width: "10%" // 缩小比例
+		}, function() {
+			successHandler(existPath, savePath);
+		}, function() {
+			faildHandler();
+		});
+	},
 }
