8a75d30fd4782063f19e5026386c0a515179afd9
kaixuan <765668947@qq.com>
2015-11-03 19:32:05 +0800

    删除资源大换

--- TravelAI/js/lvai.base/JsonTools.js
+++
@@ -1,9 +0,0 @@
-var JsonTools=function () {
-
-     this.jsonObjToString = function(jsonObj){ 
-		 return JSON.stringify(jsonObj);
-	 } 
-	 this.stringToJson = function(JsonString){ 
-		 return  eval('('+JsonString+')');
-	 } 
-} 
