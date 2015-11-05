1f2884dfe9b765d6deb1d5b7a1e551c2e6f5546b
NewsNing <newsning@sina.com>
2015-11-05 16:28:59 +0800

    新增个人相册事件，暂未实现网络与本地同步。

--- TravelAI/js/lvai.data/UserDAL.js
+++ TravelAI/js/lvai.data/UserDAL.js
@@ -394,7 +394,7 @@ var UserDAL = function() {
 			function(t, status) { //上传完成
 				console.log(status);
 				if (status == 200) {
-					successHandler(t.responseText);
+					successHandler(t.responseText,filenames);
 				} else {
 					faildHandler(status);
 				}
--- TravelAI/js/lvai.data/config.js
+++ TravelAI/js/lvai.data/config.js
@@ -17,13 +17,13 @@
 //
 
 //服务器webService地址
-var WebServiceURL="http://liu765668947.xicp.net:22217/DS.asmx/";//"/GWS.asmx/"; //"http://127.0.0.1:8037/GWS.asmx/";
+var WebServiceURL="http://192.168.1.188:13333/DS.asmx/";//"/GWS.asmx/"; //"http://127.0.0.1:8037/GWS.asmx/";
 
 //资源下载的服务器地址
-var ResourcesServerUrl = "http://liu765668947.xicp.net:22217";
-
+var ResourcesServerUrl = "http://192.168.1.188:13333";
+ 
 //资源更新等服务器地址
-var UploadServer="http://liu765668947.xicp.net:22217/UploadHandler.ashx";
+var UploadServer="http://192.168.1.188:13333/UploadHandler.ashx";
 
 //验证码的位数
 var configCodeNumber = 4;
